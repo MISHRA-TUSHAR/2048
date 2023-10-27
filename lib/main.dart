671cdc71a36b1da0d058fb1bb41fcdb30063394f
Tushar <mishratushar8890@gmail.com>
2023-10-27 11:58:58 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: '2048',
+      theme: new ThemeData(primarySwatch: Colors.blue, fontFamily: 'StarJedi'),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/mycolor.dart
@@ -0,0 +1,11 @@
+class MyColor {
+  static int gridBackground = 0xFFFA3D03;
+  static int gridColorTwoFour = 0xFFeee4da;
+  static int fontColorTwoFour = 0xFFDB832B;
+  static int emptyGridBackground = 0xFF0BAF5D;
+  static int gridColorEightSixtyFourTwoFiftySix = 0xFFf5b27e;
+  static int gridColorOneTwentyEightFiveOneTwo = 0xFFf77b5f;
+  static int gridColorSixteenThirtyTwoOneZeroTwoFour = 0xFFecc402;
+  static int gridColorWin = 0xFF60d992;
+  static int transparentWhite = 0x80FFFFFF;
+}
